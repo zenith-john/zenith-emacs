4e88857795f73e774741996ba2faaa473dcf87bc
Zenith John <zenith-john@foxmail.com>
2020-03-12 19:36:49 +0800

    Remove use-package in init-git.el and init-pyim.el.

--- config/init-git.el
+++ config/init-git.el
@@ -7,48 +7,41 @@
 
 ;; magit
 ;; dependencies: async dash git-commit transient with-editor
-(use-package magit
-  :defer 2
-  :commands
-  (magit-status
-   magit-stage-file
-   magit-unstage-file
-   magit-find-file
-   magit-init
-   magit-dispatch
-   magit-stash-both)
-  :config
-  (defun magit-stage-this-file ()
-    "Stage current buffer for magit"
-    (interactive)
-    (magit-stage-file (buffer-file-name))))
+(zenith/autoload  '(magit-status
+                    magit-stage-file
+                    magit-unstage-file
+                    magit-find-file
+                    magit-init
+                    magit-dispatch
+                    magit-stash-both) "magit")
+
+(defun magit-stage-this-file ()
+  "Stage current buffer for magit"
+  (interactive)
+  (magit-stage-file (buffer-file-name)))
 
 ;; emacs-git-gitter
-(use-package git-gutter
-  :defer 1
-  :config
-  ;; emacs-git-gutter-fringe
-  ;; dependencies: git-gutter fringe-helper
-  (require 'git-gutter-fringe)
-  ;; Configuration from https://github.com/hlissner/doom-emacs/ git-gutter module
-  (fringe-mode '4)
-  (setq-default fringes-outside-margins t)
-  ;; thin fringe bitmaps
-  (define-fringe-bitmap 'git-gutter-fr:added [224]
-    nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:modified [224]
-    nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
-    nil nil 'bottom)
-  (global-git-gutter-mode +1))
+(require 'git-gutter)
+;; emacs-git-gutter-fringe
+;; dependencies: git-gutter fringe-helper
+(require 'git-gutter-fringe)
+;; Configuration from https://github.com/hlissner/doom-emacs/ git-gutter module
+(fringe-mode '4)
+(setq-default fringes-outside-margins t)
+;; thin fringe bitmaps
+(define-fringe-bitmap 'git-gutter-fr:added [224]
+  nil nil '(center repeated))
+(define-fringe-bitmap 'git-gutter-fr:modified [224]
+  nil nil '(center repeated))
+(define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
+  nil nil 'bottom)
+(global-git-gutter-mode +1)
 
 ;; git-timemachine
 ;; dependencies: transient
-(use-package git-timemachine
-  :defer 1
-  :commands
-  (git-timemachine
-   git-timemachine-toggle))
+(zenith/autoload
+  '(git-timemachine
+    git-timemachine-toggle) "git-timemachine")
 
 (provide 'init-git)
 ;;; init-git.el ends here
--- config/init-pyim.el
+++ config/init-pyim.el
@@ -6,33 +6,30 @@
 ;;; Code:
 
 ;; pyim
-(use-package pyim
-  :general ("M-/" 'pyim-convert-string-at-point)
-  :init
-  (setq pyim-dcache-directory (concat zenith-emacs-local-dir "pyim/dcache/")
-        pyim-page-tooltip 'posframe
-        default-input-method "pyim")
-  :config
-  ;; liberime
-  (use-package liberime
-    :load-path "~/zenith-emacs/extensions/liberime/build/liberime.so"
-    :config
-    (liberime-start (expand-file-name "/usr/share/rime-data/")
-                    (concat zenith-emacs-root-dir "local/pyim/rime/"))
-    (liberime-select-schema "luna_pinyin_simp")
-    (setq pyim-default-scheme 'rime))
 
-  (require 'pyim)
-  (pyim-isearch-mode 1)
-  (add-to-list 'pyim-punctuation-dict '("\\" "、"))
-  (setq-default pyim-english-input-switch-functions
-                '(pyim-probe-dynamic-english
-                  pyim-probe-isearch-mode
-                  pyim-probe-program-mode))
+(require 'pyim)
 
-  (setq-default pyim-punctuation-half-width-functions
-                '(pyim-probe-punctuation-line-beginning
-                  pyim-probe-punctuation-after-punctuation)))
+(general-define-key "M-/" 'pyim-convert-string-at-point)
+(setq pyim-dcache-directory (concat zenith-emacs-local-dir "pyim/dcache/")
+      pyim-page-tooltip 'posframe
+      default-input-method "pyim")
+;; liberime
+(load-file (expand-file-name "liberime/build/liberime.so" zenith-emacs-extension-dir))
+(liberime-start (expand-file-name "/usr/share/rime-data/")
+                (concat zenith-emacs-root-dir "local/pyim/rime/"))
+(liberime-select-schema "luna_pinyin_simp")
+(setq pyim-default-scheme 'rime)
+
+(pyim-isearch-mode 1)
+(add-to-list 'pyim-punctuation-dict '("\\" "、"))
+(setq-default pyim-english-input-switch-functions
+              '(pyim-probe-dynamic-english
+                pyim-probe-isearch-mode
+                pyim-probe-program-mode))
+
+(setq-default pyim-punctuation-half-width-functions
+              '(pyim-probe-punctuation-line-beginning
+                pyim-probe-punctuation-after-punctuation))
 
 
 (provide 'init-pyim)
