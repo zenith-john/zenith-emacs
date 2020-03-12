cd05843172f48dc5405ddd0641c5e4ac3e196dea
Zenith John <zenith-john@foxmail.com>
2020-03-12 18:27:30 +0800

    Update init file.

--- init.el
+++ init.el
@@ -9,28 +9,11 @@
 (defvar zenith-emacs-extension-dir (expand-file-name "extensions/" zenith-emacs-root-dir))
 (defvar zenith-emacs-config-dir (expand-file-name "config/" zenith-emacs-root-dir))
 (defvar zenith-emacs-local-dir (expand-file-name "local/" zenith-emacs-root-dir))
-(defvar zenith-file-name-handler-alist file-name-handler-alist "Remember the origin value of file-name-handler-alist")
 
-(setq file-name-handler-alist nil)
-(setq frame-inhibit-implied-resize nil)
-
-(require 'cl-lib)
-;; From https://emacs-china.org/t/topic/3931/2
-(defun eh-hack-load-path ()
-  ;; Delete buildin org's PATH
-  (setq load-path
-        (cl-remove-if
-         #'(lambda (path)
-             (string-match "lisp/org$" path))
-         load-path))
-  ;; Demove property lists to defeat cus-load and remove autoloads
-  (mapatoms
-   #'(lambda (sym)
-       (let ((sym-name (symbol-name sym)))
-         (when (string-match "^\\(org\\|ob\\|ox\\)-?" sym-name)
-           (setplist sym nil)
-           (when (autoloadp sym)
-             (unintern sym)))))))
+;; Do not load package
+(setq initial-major-mode 'fundamental-mode
+      package-enable-at-startup nil
+      frame-inhibit-implied-resize t)
 
 (defun add-subdirs-to-load-path (dir)
   "Recursive add directories to `load-path'."
@@ -38,19 +21,20 @@
     (add-to-list 'load-path dir)
     (normal-top-level-add-subdirs-to-load-path)))
 
-(eh-hack-load-path)
-(add-to-list 'load-path zenith-emacs-config-dir)
-(add-subdirs-to-load-path zenith-emacs-extension-dir)
+(let (
+      ;; Reduce GC to speed up initialization.
+      (gc-cons-threshold most-positive-fixnum)
+      (gc-cons-percentage 0.6)
+      ;; Do not load major mode
+      (file-name-handler-alist nil))
 
-;; benchmark-init-el
-(require 'benchmark-init)
-(benchmark-init/activate)
-(add-hook 'after-init-hook 'benchmark-init/deactivate)
-(require 'config)
+  (add-to-list 'load-path zenith-emacs-config-dir)
+  (add-subdirs-to-load-path zenith-emacs-extension-dir)
 
-(add-hook 'after-init-hook (lambda ()(setq file-name-handler-alist zenith-file-name-handler-alist
-                                           gc-cons-threshold 16777216
-                                           gc-cons-percentage 0.6)))
+  (require 'benchmark-init)
+  (benchmark-init/activate)
+  (add-hook 'after-init-hook 'benchmark-init/deactivate)
+  (require 'config))
 
 (require 'server)
 (unless (server-running-p)
