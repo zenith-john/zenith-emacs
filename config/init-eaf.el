308069ef0e1ccd7b908050b455e01b5b6b504841
Zenith John <zenith-john@foxmail.com>
2020-03-31 09:02:00 +0800

    Tweaks emacs-application-framework.

--- config/init-eaf.el
+++ config/init-eaf.el
@@ -6,8 +6,13 @@
 ;;; Code:
 
 ;; emacs-application-framework
-(autoload 'eaf-open "eaf")
+(autoload 'eaf-open "eaf" nil t)
 (setq eaf-find-alternate-file-in-dired t)
+
+(setq browse-url-browser-function 'eaf-open-browser)
+(defalias 'browse-web #'eaf-open-browser)
+
+(eaf-setq eaf-pdf-dark-mode "false")
 
 (provide 'init-eaf)
 ;;; init-eaf.el ends here
