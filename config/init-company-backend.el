93842a7bf2a1f07a970e2ffa8059731002e8abaf
Zenith John <zenith-john@foxmail.com>
2020-07-22 22:08:44 +0800

    Fix some bug in company-combine.

--- config/init-company-backend.el
+++ config/init-company-backend.el
@@ -12,6 +12,16 @@
 (unless (require 'fuz-core nil t)
   (fuz-build-and-load-dymod))
 
+(defvar company-combine-common-backends '(company-capf)
+  "Company backends for common buffers")
+
+(defvar-local company-combine-local-backends company-combine-common-backends
+  "Company backends used for matching")
+;; The matching is not usable in mode other than emacs-list-mode
+;; As some backends rely on environment to provide the completion.
+(defvar-local company-combine-matching-prefix ""
+  "Prefix for matching")
+
 ;; See https://www.gnu.org/software/emacs/manual/html_node/elisp/Defining-Hash.html
 (defun company-combine-case-fold-string= (a b)
   (string-equal (upcase a) (upcase b)))
@@ -33,39 +43,30 @@
   ;; Sort candidate alphabetically and then show the candidates with same prefix
   ;; first
   (clrhash company-combine-sort-hashtable)
-  (let ((company-prefix (company-combine-get-prefix)))
-    (dolist (candidate candidates)
-      (puthash candidate (or (and company-prefix (fuz-calc-score-skim company-prefix candidate)) 0) company-combine-sort-hashtable))
-    (delete-consecutive-dups (sort candidates #'company-combine-compare-string))
-    ))
+  (dolist (candidate candidates)
+    (puthash candidate
+             (or (and company-combine-matching-prefix
+                      (fuz-calc-score-skim company-combine-matching-prefix candidate)) 0)
+             company-combine-sort-hashtable))
+  (delete-consecutive-dups (sort candidates #'company-combine-compare-string)))
 
-
-(defvar company-combine-common-backends '(company-capf)
-  "Company backends for common buffers")
-
-(defvar-local company-combine-local-backends company-combine-common-backends
-  "Company backends used for matching")
-;; The matching is not usable in mode other than emacs-list-mode
-;; As some backends rely on environment to provide the completion.
-(defvar-local company-combine-matching-prefix ""
-  "Prefix for matching")
-
-;; Use my own prefix function to replace TabNine's
 (defun company-combine-get-prefix ()
   (interactive)
-  (let ((prefix "") (new-prefix) (len 0) (new-len 0))
+  (let ((prefix "") (new-prefix) (len 200) (new-len 0)) ; I think no word is longer than 200
     (dolist (backend company-combine-local-backends)
       (setq new-prefix (funcall backend 'prefix))
       (when new-prefix
         (cond
-         ((eq new-prefix 'stop) (setq new-len 0))
+         ((eq new-prefix 'stop) (setq new-len 200))
          ((cdr-safe new-prefix)
           (setq new-len (cdr new-prefix)))
-         (t (setq new-len (length new-prefix)))))
-      (when (< new-len len)
-        (setq prefix new-prefix
-              len new-len)))
-    prefix))
+         (t (setq new-len (length new-prefix))))
+        (when (< new-len len)
+          (setq prefix new-prefix
+                len new-len))))
+    (if (< len 200)
+        prefix
+      nil)))
 
 ;; the following code is inspired by company-fuzzy
 (defun company-combine-get-annotation (candidate)
@@ -94,12 +95,6 @@
 (defun company-combine-matching-candidate (candidate)
   (fuz-calc-score-skim company-combine-matching-prefix (substring-no-properties candidate)))
 
-(defun company-combine-extract-prefix (prefix)
-  (cond
-   ((or (not prefix) (equal prefix 'stop)) "")
-   ((listp prefix) (car prefix))
-   (t prefix)))
-
 (defun company-combine-get-candidates (backend)
   (save-excursion
     (when-let* ((prefix (funcall backend 'prefix))
@@ -117,8 +112,7 @@
           (setq temp-candidates (company-combine-get-candidates backend))
           (when temp-candidates
             (mapc (lambda (arg)(put-text-property 0 1 'company-backend backend arg)) temp-candidates)
-            (setq all-candidates (append all-candidates temp-candidates))
-            ))))
+            (setq all-candidates (append all-candidates temp-candidates))))))
     (company-combine-transformer all-candidates)))
 
 (defun company-combine-post-completion (result)
@@ -143,14 +137,14 @@
   (cl-case command
     (interactive (company-begin-backend 'company-combine))
     (prefix
-     (setq company-combine-matching-prefix (company-combine-get-prefix))
      (and (not (company-in-string-or-comment))
-          (company-grab-symbol)))
+          (setq company-combine-matching-prefix (company-combine-get-prefix))))
     (candidates (company-combine-candidates))
     (annotation (company-combine-get-annotation arg))
     (doc-buffer (company-combine-get-doc arg))
     (post-completion (company-combine-post-completion arg))
-    (sorted t)))
+    (sorted t)
+    (no-cache t)))
 
 (defun company-combine-set-company-backends ()
   ;; Set backends for company-mode
